6e2565cdb79b74d0b377b3da4c5793a19333be66
Link-djst <diego.stt@live.com>
2015-11-14 10:48:38 -0600

    Last
    En un cola meto los botones que hemos presionado, y así podemos buscar
    entre dos botones el camino más corto.

--- Last(2)/Graph.java
+++ Last(2)/Graph.java
@@ -1,10 +1,9 @@
 
-import java.util.Vector;
-
 public class Graph {
 	private  Vertex[] setVertex;
 	private Integer[][] adyacencyMatrix;
-	private int x,y,size,i,j,current;
+	private int x,y,size,i,j,k,current;
+	private Vertex v;
 	private String line;
 	
 	public Graph(String x, String y){
@@ -52,14 +51,10 @@ public class Graph {
 			for(j=0;j<size;j++){
 				current = this.setVertex[i].getNumber();
 				try{
-					if((current%this.x)!=this.x-1){
-						this.adyacencyMatrix[current][current+1] = 1;
-					}
+					this.adyacencyMatrix[current][current+1] = 1;
 				}catch(Exception e){
 				}try{
-					if((current%this.x)!=0){
-						this.adyacencyMatrix[current][current-1] = 1;
-					}
+					this.adyacencyMatrix[current][current-1] = 1;
 				}catch(Exception e){
 				} try{
 					this.adyacencyMatrix[current][current+this.x] = 1;
@@ -76,20 +71,8 @@ public class Graph {
 	public Vertex[] getSetVertex(){
 		return setVertex;
 	}
-	public Integer[] getAdyacencyList(int vertex){
-			String index ="";
-			Integer[] dummy = adyacencyMatrix[vertex];
-			Vector<Integer> dummy2 = new Vector<Integer>();
-			for(int i = 0; i< size; i++){
-				if(dummy[i]==1){
-					dummy2.add(i);
-					index = index +" "+ i;
-    		}
-    	}
-    	System.out.println(vertex+" adyacent to "+index);
-    	Integer[] adyacencyList = new Integer[dummy2.size()];
-    	dummy2.toArray(adyacencyList);
-		return adyacencyList;
+	public Integer[] getAdyacency(int vertex){
+		return adyacencyMatrix[vertex];
 	}
 	
 }
--- Last/Edge.java
+++ Last/Edge.java
@@ -6,7 +6,7 @@ public class Edge {
 	private boolean enabled;
 	
 	Edge(){
-		this.weight = 0;
+		this.setWeight(0);
 		enabled = true;
 	}
 	
@@ -19,6 +19,30 @@ public class Edge {
 	}
 	
 	public void Weight(int w){
-		this.weight = w;
+		this.setWeight(w);
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public Vertex getV1() {
+		return v1;
+	}
+
+	public void setV1(Vertex v1) {
+		this.v1 = v1;
+	}
+
+	public Vertex getV2() {
+		return v2;
+	}
+
+	public void setV2(Vertex v2) {
+		this.v2 = v2;
 	}
 }
