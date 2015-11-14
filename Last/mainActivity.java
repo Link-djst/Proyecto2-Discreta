83fe8f77783fbd1732fe1097d73c01b4f5ce46fe
Link-djst <diego.stt@live.com>
2015-11-13 18:39:08 -0600

    Last
    Usa javafx para crear botones. Además, ya muestra la lista de adyacencia
    de cada botón. Solo que la matriz de adyacencia esta mala, así que hay
    que arreglarla para poder seguir trabajando.
    
    Graph.java/AdyacencyMatrix debe de ser modificada para arreglar ese
    problema. Se deben de considerar varios casos, asi como las bandas o las
    partes interiores del laberinto.

---
+++ Last/mainActivity.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class mainActivity extends Application {
+	private Button cBtn;
+	private Vertex[] setVertex;
+	private TextField x;
+	private TextField y;
+	private StackPane frame;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		cBtn = new Button("Create Matrix");
+		x = new TextField("X: ");
+		y = new TextField("Y: ");
+		HBox hb = new HBox();
+		hb.getChildren().addAll(x,y,cBtn);
+		frame = new StackPane();
+		frame.getChildren().add(hb);
+		Scene scene = new Scene(frame, 700, 625);
+		
+		cBtn.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override 
+		    public void handle(ActionEvent e) {
+		    	initGraph();
+		    }
+		});
+		
+		primaryStage.resizableProperty().setValue(false);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Proyecto Discreta 2");
+		primaryStage.show();
+	}
+	
+	private void initGraph(){
+		FlowPane fp = new FlowPane();
+		Graph G = new Graph(x.getText(), y.getText());
+		this.setVertex = G.getSetVertex();
+		for(int i = 0;i < setVertex.length;i++){
+			setVertex[i].setOnAction(new EventHandler<ActionEvent>() {
+			    @Override 
+			    public void handle(ActionEvent e) {
+			    	int current = ((Vertex) e.getSource()).getNumber();
+			    	int i=0;
+			    	String index = "";
+			    	while(i>=0){
+			    		i = Arrays.toString(G.getAdyacency(current)).indexOf("1", i+1);
+			    		if(i != -1){
+			    			index = index +" "+ i;	
+			    		}
+			    	}
+			    	System.out.println(current+" adyacent to "+index);
+			    }
+			});
+			fp.getChildren().add(setVertex[i]);
+		}
+		frame.getChildren().add(fp);
+		
+	}
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+
+}
